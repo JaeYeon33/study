07bfc611287d2d50b4aa178591262b6139977d37
JaeYeon33 <cjyeon1022@gmail.com>
2021-11-03 21:07:28 +0900

    file

--- src/main/java/me/jaeyeon/events/EventController.java
+++ src/main/java/me/jaeyeon/events/EventController.java
@@ -1,21 +1,31 @@
 package me.jaeyeon.events;
 
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.net.URI;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
 
-    @PostMapping("/api/events")
-    public ResponseEntity createEvent() {
+    private final EventRepository eventRepository;
+    private final ModelMapper modelMapper;
 
-        URI createdUri = linkTo(methodOn(EventController.class).createEvent()).slash("{id}").toUri();
-        return ResponseEntity.created(createdUri).build();
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+        Event event = modelMapper.map(eventDto, Event.class);
+        Event newEvent = this.eventRepository.save(event);
+        URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
+        return ResponseEntity.created(createdUri).body(event);
     }
 }
--- src/main/java/me/jaeyeon/study/StudyApplication.java
+++ src/main/java/me/jaeyeon/study/StudyApplication.java
@@ -1,7 +1,9 @@
 package me.jaeyeon.study;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StudyApplication {
@@ -10,4 +12,8 @@ public class StudyApplication {
 		SpringApplication.run(StudyApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
